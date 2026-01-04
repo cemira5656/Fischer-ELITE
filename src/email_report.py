a7e7674ad395fba5d8964ce97d266b230affbd89
cemira5656 <shawlogic@gmail.com>
2026-01-04 14:40:05 +0100

    Create email_report.py

---
+++ src/email_report.py
@@ -0,0 +1,19 @@
+import os
+import smtplib
+from email.message import EmailMessage
+
+def send_gmail(subject: str, body_text: str):
+    gmail_user = os.environ["GMAIL_USER"]
+    gmail_pass = os.environ["GMAIL_APP_PASSWORD"]
+    to_email = os.environ["EMAIL_TO"]
+
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = gmail_user
+    msg["To"] = to_email
+    msg.set_content(body_text)
+
+    # Gmail SMTP
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+        smtp.login(gmail_user, gmail_pass)
+        smtp.send_message(msg)
